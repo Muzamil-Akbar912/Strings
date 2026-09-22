ccafc68830c959345c3711e64d97e4f32ba4b117
Muzamil-Akbar912 <darmuzamil006@gmail.com>
2026-09-22 17:56:56 +0530

    Add new Question

---
+++ PalindromeCheck.java
@@ -0,0 +1,18 @@
+public class PalindromeCheck {
+    public static boolean isPalindrome(String str) {
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) != str.charAt(str.length()-1-i)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+    public static void main(String[] args) {
+        String str = "racecar"; // true
+
+        // String str = "racecarz"; // false
+        
+        System.out.println(isPalindrome(str));
+    }
+}
