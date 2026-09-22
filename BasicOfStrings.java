26798dcbd82ba3ea2a70ae56b28799eb94bbbd24
Muzamil-Akbar912 <darmuzamil006@gmail.com>
2026-09-22 08:43:02 +0530

    Basics of Strings

---
+++ BasicOfStrings.java
@@ -0,0 +1,30 @@
+// import java.util.*;
+public class BasicOfStrings {
+    public static void main(String[] args) {
+        // char arr[] = {'a', 'b', 'c', 'd'};
+        // String str = "abcd";
+        // String str2 = new String("xyz");
+
+        // Strings are IMMUTABLE...
+
+        // Scanner sc = new Scanner(System.in);
+        // String name = sc.next(); // yeh single word layta hai...
+        // System.out.println(name);
+
+        // String name2 = sc.nextLine(); // yeh poore line layta hai...
+        // System.out.println(name2);
+
+        // Length of string...
+        // String fullName = "Muzamil Akbar";
+        // System.out.println(fullName.length());
+
+        // Concatenation....
+        String firstName = "Muzamil";
+        String lastName = "Akbar";
+        String fullName = firstName + " " + lastName;
+        System.out.println(fullName);
+
+        // CharAt method...
+        System.out.println(fullName.charAt(0));
+    }
+}
