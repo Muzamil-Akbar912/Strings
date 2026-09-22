53420c8a6c77fdd254d6d93248acd1532bd676b5
Muzamil-Akbar912 <darmuzamil006@gmail.com>
2026-09-22 17:43:32 +0530

    Add new Question

---
+++ PrintLetters.java
@@ -0,0 +1,9 @@
+public class PrintLetters {
+    public static void main(String[] args) {
+        String name = "ApnaCollege";
+
+        for (int i = 0; i < name.length(); i++) {
+            System.out.print(name.charAt(i) + " ");
+        }
+    }
+}
